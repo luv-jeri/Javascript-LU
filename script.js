87f545c4e4a85053420cddfb703d5e49f8bee7ce
luv-jeri <luvvjeri@gmail.com>
2022-11-02 23:10:45 +0530

    -

--- script.js
+++ script.js
@@ -23,18 +23,37 @@ class Emogie {
   remove() {
     this.box.remove();
   }
+
+  animate() {
+    this.box.classList.add('animate');
+    setTimeout(() => {
+      this.box.classList.remove('animate');
+    }, 1000);
+  }
+
+  increaseSize(times) {
+    this.box.style.transform = `scale(${times})`;
+  }
 }
 
 const e1 = new Emogie('smile', '😀');
 const e2 = new Emogie('Cry', '🥲');
 const e3 = new Emogie('Laugh', '😂');
+const e4 = new Emogie('Angry', '😡');
 
 e1.render();
 e2.render();
 e3.render();
+e4.render();
+e4.chanegColor('red');
+
+e1.increaseSize(1.1)
 
 e1.chanegColor('red');
 e2.chanegColor('#82CD47');
 
 e3.remove();
 e3.render();
+
+e4.animate();
+e1.animate();
